75d27105459b243df398ad2763ec79147816d864
kevin-koehler <kevin.koehler@usask.ca>
2020-03-26 12:56:24 -0600

    basic string hashmap works

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+"use-strict";
+
 const HASH_SIZE = 64,
   FNV_PRIME_64 = 1099511628211n,
   FNV_OFFSET_64 = 14695981039346656037n;
 
+let char;
 function FNV1aHash64(string) {
   const { length } = string;
-  let char,
-    hash = FNV_OFFSET_64,
+  let hash = FNV_OFFSET_64,
     i = 0;
   for (
     ;
@@ -18,11 +20,22 @@ function FNV1aHash64(string) {
 
 const DEFAULT_TABLE_SIZE_L_SHIFT = 9;
 
+function fastRemoveElem(arr, i) {
+  const tmp = arr[arr.length - 1];
+  arr[arr.length - 1] = arr[i];
+  arr[i] = tmp;
+  arr.pop();
+}
+
+// TODO: use mersenne primes for table size
+// TODO: handle grow
+// TODO: values method
 class FastStringMap {
   constructor() {
     this._size_factor = DEFAULT_TABLE_SIZE_L_SHIFT;
-    this._capacity = BigInt(0x2 << this._size_factor);
-    this._table = new Array(this._capacity);
+    const capacity = 0x2 << this._size_factor;
+    this._capacity = BigInt(capacity);
+    this._table = new Array(capacity);
     this.size = 0;
   }
 
@@ -31,21 +44,59 @@ class FastStringMap {
   }
 
   /**
-   * @param key the key to get
+   * @param key unique key
    * @returns the value stored in the key, or undefined if it doesnt exist
    */
   get(key) {
-    return this._table[this._indexOfKey(key)];
+    const bucket = this._table[this._indexOfKey(key)];
+    if (!bucket) {
+      return undefined;
+    }
+    let { length } = bucket,
+      i = 0;
+    for (; i < length; ++i) {
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
+      }
+    }
+    return undefined;
   }
 
   /**
-   * @param key the unique key by which to store your value
+   * @param key unique key
    * @param value the value to store
    * @returns this
    */
   set(key, value) {
-    this._table[this._indexOfKey(key)] = value;
+    let bucket,
+      table = this._table,
+      index = this._indexOfKey(key);
+    if ((bucket = table[index]) !== undefined) {
+      bucket.push([key, value]);
+    } else {
+      table[index] = [[key, value]];
+    }
     ++this.size;
+    return this;
+  }
+
+  /**
+   * @param key unique key
+   * @returns this
+   */
+  remove(key) {
+    const bucket = this._table[this._indexOfKey(key)];
+    if (!bucket) {
+      return this;
+    }
+    let { length } = bucket,
+      i = 0;
+    for (; i < length; ++i) {
+      if (bucket[i][0] === key) {
+        fastRemoveElem(bucket, i);
+        return this;
+      }
+    }
     return this;
   }
 }
--- index.test.js
+++ index.test.js
@@ -1,19 +1,82 @@
 const Map = require(".");
 const { FNV1aHash64 } = require(".");
 
-describe("FNV1aHash128", () => {
+describe("FNV1aHash64", () => {
   it("should hash hello world properly", () => {
     expect(FNV1aHash64("hello")).toBe(11831194018420276491n);
   });
 });
 
-describe("Map", () => {
+describe("FastStringMap", () => {
   describe("get and set", () => {
     it("should get and set a string property", () => {
       const map = new Map();
       map.set("a", "b");
       expect(map.get("c")).toBeUndefined();
       expect(map.get("a")).toBe("b");
+    });
+  });
+
+  describe("remove", () => {
+    it("should remove the property", () => {
+      const map = new Map().set("foo", "bar");
+      expect(map.get("foo")).toBe("bar");
+      map.remove("foo");
+      expect(map.get("foo")).toBeUndefined();
+    });
+  });
+
+  describe("handles collisions properly", () => {
+    it("should get the right value", () => {
+      {
+        const map = new Map();
+        map
+          .set("8yn0iYCKYHlIj4-BwPqk", "foo")
+          .set("GReLUrM4wMqfg9yzV3KQ", "bar");
+        expect(map._indexOfKey("8yn0iYCKYHlIj4-BwPqk")).toBe(
+          map._indexOfKey("GReLUrM4wMqfg9yzV3KQ")
+        );
+        expect(map.get("8yn0iYCKYHlIj4-BwPqk")).toBe("foo");
+        expect(map.get("GReLUrM4wMqfg9yzV3KQ")).toBe("bar");
+      }
+      {
+        const map = new Map();
+        map
+          .set("GReLUrM4wMqfg9yzV3KQ", "bar")
+          .set("8yn0iYCKYHlIj4-BwPqk", "foo");
+        expect(map._indexOfKey("8yn0iYCKYHlIj4-BwPqk")).toBe(
+          map._indexOfKey("GReLUrM4wMqfg9yzV3KQ")
+        );
+        expect(map.get("8yn0iYCKYHlIj4-BwPqk")).toBe("foo");
+        expect(map.get("GReLUrM4wMqfg9yzV3KQ")).toBe("bar");
+      }
+    });
+
+    it("should remove the right element", () => {
+      {
+        const map = new Map();
+        map
+          .set("8yn0iYCKYHlIj4-BwPqk", "foo")
+          .set("GReLUrM4wMqfg9yzV3KQ", "bar");
+        expect(map._indexOfKey("8yn0iYCKYHlIj4-BwPqk")).toBe(
+          map._indexOfKey("GReLUrM4wMqfg9yzV3KQ")
+        );
+        map.remove("8yn0iYCKYHlIj4-BwPqk");
+        expect(map.get("8yn0iYCKYHlIj4-BwPqk")).toBeUndefined();
+        expect(map.get("GReLUrM4wMqfg9yzV3KQ")).toBe("bar");
+      }
+      {
+        const map = new Map();
+        map
+          .set("8yn0iYCKYHlIj4-BwPqk", "foo")
+          .set("GReLUrM4wMqfg9yzV3KQ", "bar");
+        expect(map._indexOfKey("8yn0iYCKYHlIj4-BwPqk")).toBe(
+          map._indexOfKey("GReLUrM4wMqfg9yzV3KQ")
+        );
+        map.remove("GReLUrM4wMqfg9yzV3KQ");
+        expect(map.get("8yn0iYCKYHlIj4-BwPqk")).toBe("foo");
+        expect(map.get("GReLUrM4wMqfg9yzV3KQ")).toBeUndefined();
+      }
     });
   });
 });
