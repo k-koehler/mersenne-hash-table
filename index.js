7a4708ac2cd94edc01c46ad6b6fb8b59494d26ca
kevin-koehler <kevin.koehler@usask.ca>
2020-03-26 12:00:35 -0600

    can set and get basic properties, does not handle collisions

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+const HASH_SIZE = 64,
+  FNV_PRIME_64 = 1099511628211n,
+  FNV_OFFSET_64 = 14695981039346656037n;
+
+function FNV1aHash64(string) {
+  const { length } = string;
+  let char,
+    hash = FNV_OFFSET_64,
+    i = 0;
+  for (
+    ;
+    i < length;
+    char = BigInt(string.charCodeAt(i++)),
+      hash = BigInt.asUintN(HASH_SIZE, (hash ^ char) * FNV_PRIME_64)
+  );
+  return hash;
+}
+
+const DEFAULT_TABLE_SIZE_L_SHIFT = 9;
+
 class FastStringMap {
+  constructor() {
+    this._size_factor = DEFAULT_TABLE_SIZE_L_SHIFT;
+    this._capacity = BigInt(0x2 << this._size_factor);
+    this._table = new Array(this._capacity);
+    this.size = 0;
+  }
+
+  _indexOfKey(key) {
+    return FNV1aHash64(key) & (this._capacity - 1n);
+  }
+
   /**
    * @param key the key to get
    * @returns the value stored in the key, or undefined if it doesnt exist
    */
   get(key) {
-    return undefined;
+    return this._table[this._indexOfKey(key)];
   }
 
   /**
@@ -13,8 +44,11 @@ class FastStringMap {
    * @returns this
    */
   set(key, value) {
+    this._table[this._indexOfKey(key)] = value;
+    ++this.size;
     return this;
   }
 }
 
 module.exports = FastStringMap;
+module.exports.FNV1aHash64 = FNV1aHash64;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,11 @@
 const Map = require(".");
+const { FNV1aHash64 } = require(".");
+
+describe("FNV1aHash128", () => {
+  it("should hash hello world properly", () => {
+    expect(FNV1aHash64("hello")).toBe(11831194018420276491n);
+  });
+});
 
 describe("Map", () => {
   describe("get and set", () => {
