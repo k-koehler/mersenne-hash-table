fbabc7ece70169f82601bdfee3ead18675fc9599
kevin-koehler <kevin.koehler@usask.ca>
2020-03-26 18:03:57 -0600

    added benchmarks

---
+++ bench/large-dataset.js
@@ -0,0 +1,68 @@
+const FastStringMap = require("fast-string-map");
+const { now } = require("microtime");
+
+const DATA_SIZE = 1000000;
+
+const map = new Map(),
+  fastStringMap = new FastStringMap(),
+  obj = {};
+function setGetFastStringMap() {
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    fastStringMap.set(`${i}`, i);
+  }
+  let result = 0;
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    result += fastStringMap.get(`${i}`);
+  }
+  return result;
+}
+
+function setGetMap() {
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    map.set(`${i}`, i);
+  }
+  let result = 0;
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    result += map.get(`${i}`);
+  }
+  return result;
+}
+
+function setGetObject() {
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    obj[i] = i;
+  }
+  let result = 0;
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    result += obj[i];
+  }
+  return result;
+}
+
+let t1, t2, t3;
+t1 = t2 = t3 = 0;
+for (let i = 0; i < 10; ++i) {
+  {
+    const start = now();
+    setGetMap();
+    const end = now();
+    t1 += end - start;
+  }
+  {
+    const start = now();
+    setGetObject();
+    const end = now();
+    t2 += end - start;
+  }
+  {
+    const start = now();
+    setGetFastStringMap();
+    const end = now();
+    t3 += end - start;
+  }
+}
+
+console.log("Total time in microseconds (less is better):");
+console.log("Map", t1);
+console.log("Object", t2);
+console.log("FastStringMap", t3);
---
+++ bench/med-dataset.js
@@ -0,0 +1,69 @@
+const FastStringMap = require("fast-string-map");
+const { now } = require("microtime");
+const assert = require("assert");
+
+const DATA_SIZE = 10000;
+
+const map = new Map(),
+  fastStringMap = new FastStringMap(),
+  obj = {};
+function setGetFastStringMap() {
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    fastStringMap.set(`${i}`, i);
+  }
+  let result = 0;
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    result += fastStringMap.get(`${i}`);
+  }
+  return result;
+}
+
+function setGetMap() {
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    map.set(`${i}`, i);
+  }
+  let result = 0;
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    result += map.get(`${i}`);
+  }
+  return result;
+}
+
+function setGetObject() {
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    obj[i] = i;
+  }
+  let result = 0;
+  for (let i = 0; i < DATA_SIZE; ++i) {
+    result += obj[i];
+  }
+  return result;
+}
+
+let t1, t2, t3;
+t1 = t2 = t3 = 0;
+for (let i = 0; i < 10; ++i) {
+  {
+    const start = now();
+    assert.equal(setGetMap(), 49995000);
+    const end = now();
+    t1 += end - start;
+  }
+  {
+    const start = now();
+    assert.equal(setGetObject(), 49995000);
+    const end = now();
+    t2 += end - start;
+  }
+  {
+    const start = now();
+    assert.equal(setGetFastStringMap(), 49995000);
+    const end = now();
+    t3 += end - start;
+  }
+}
+
+console.log("Total time in microseconds (less is better):");
+console.log("Map", t1);
+console.log("Object", t2);
+console.log("FastStringMap", t3);
